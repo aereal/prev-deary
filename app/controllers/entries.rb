3b5bf5cd1690f5810dfa93ee82700e76c6e9f62a
aereal <aereal@aereal.org>
2013-11-06 17:40:36 +0900

    Show entries

--- app/controllers/entries.rb
+++ app/controllers/entries.rb
@@ -1,3 +1,12 @@
 Deary::App.controllers :entries do
+  get :index, '/' do
+    @entries = Entry.order(:created_at).reverse
+    render 'entries/index'
+  end
 
+  get :show, %r{/\d{4}/\d{2}/\d{2}/\d{6}} do
+    @entry = Entry.where(path: request.path).first or halt 404
+    @page_title = @entry.title
+    render 'entries/show'
+  end
 end
--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,7 +1,34 @@
 # Helper methods defined here can be accessed in any controller or view in the application
 
 Deary::App.helpers do
-  # def simple_helper_method
-  #  ...
-  # end
+  def article_tag(options = {}, &block)
+    unless article_permalink?
+      options = options.merge(
+        itemscope: true,
+        itemtype:  article_type,
+        itemprop:  :BlogPost,
+      )
+    end
+    content_tag(:article, options, &block)
+  end
+
+  def article_permalink?
+    (request.controller == 'entries') && (request.action == :show)
+  end
+
+  def article_type
+    article_permalink? ? :Article : :BlogPosting
+  end
+
+  def page_type
+    article_permalink? ? :Blog : :Article
+  end
+
+  def site_title
+    settings.site_title
+  end
+
+  def page_title
+    [@page_title, site_title].compact.join(' | ')
+  end
 end
