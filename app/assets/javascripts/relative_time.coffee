dd64ade6b1056b8b8e8e4bf5ce33f42bd73fffb1
aereal <aereal@aereal.org>
2013-11-10 16:59:07 +0900

    Support larger range

--- app/assets/javascripts/relative_time.coffee
+++ app/assets/javascripts/relative_time.coffee
@@ -4,12 +4,25 @@ $ ->
     $time = $(@)
     datetime = new Date($time.attr('datetime'))
     delta = (now - datetime) / 1000 # milliseconds -> seconds
+
+    minute = 60
+    hour = minute * 60
+    day = hour * 24
+    week = day * 7
+    month = day * 30
+    year = day * 365
     relativeTime =
       switch
-        when delta < 60
+        when delta < minute
           "#{Math.round(delta)} seconds ago"
-        when delta < (60 * 60)
-          "#{Math.round(delta / 60)} minutes ago"
-        when delta < (60 * 60 * 24)
-          "#{Math.round(delta / (60 * 60))} hours ago"
+        when delta < hour
+          "#{Math.round(delta / minute)} minutes ago"
+        when delta < day
+          "#{Math.round(delta / hour)} hours ago"
+        when delta < week
+          "#{Math.round(delta / day)} days ago"
+        when delta < month
+          "#{Math.round delta / week} weeks ago"
+        when delta < year
+          "#{Math.round delta / month} months ago"
     $time.text(relativeTime)
--- app/assets/javascripts/relative_time.js
+++ app/assets/javascripts/relative_time.js
@@ -3,18 +3,30 @@
     var now;
     now = new Date();
     return $('time[data-relative-time]').each(function() {
-      var $time, datetime, delta, relativeTime;
+      var $time, datetime, day, delta, hour, minute, month, relativeTime, week, year;
       $time = $(this);
       datetime = new Date($time.attr('datetime'));
       delta = (now - datetime) / 1000;
+      minute = 60;
+      hour = minute * 60;
+      day = hour * 24;
+      week = day * 7;
+      month = day * 30;
+      year = day * 365;
       relativeTime = (function() {
         switch (false) {
-          case !(delta < 60):
+          case !(delta < minute):
             return "" + (Math.round(delta)) + " seconds ago";
-          case !(delta < (60 * 60)):
-            return "" + (Math.round(delta / 60)) + " minutes ago";
-          case !(delta < (60 * 60 * 24)):
-            return "" + (Math.round(delta / (60 * 60))) + " hours ago";
+          case !(delta < hour):
+            return "" + (Math.round(delta / minute)) + " minutes ago";
+          case !(delta < day):
+            return "" + (Math.round(delta / hour)) + " hours ago";
+          case !(delta < week):
+            return "" + (Math.round(delta / day)) + " days ago";
+          case !(delta < month):
+            return "" + (Math.round(delta / week)) + " weeks ago";
+          case !(delta < year):
+            return "" + (Math.round(delta / month)) + " months ago";
         }
       })();
       return $time.text(relativeTime);
