9def3cf46f5bba523c9f9e2dc14aad97c95f397d
aereal <aereal@aereal.org>
2013-11-19 18:48:31 +0900

    Add spec for User#authenticate!

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe User do
-  pending
+  describe '#authenticate!' do
+    context 'with valid password' do
+      let(:name) { 'internet' }
+      let(:password) { 'dankogai' }
+      let(:valid_password) { password }
+      let(:user) { User.create_with_digest(name: name, password: password) }
+
+      it "succeeds authentication" do
+        expect { user.authenticate!(valid_password) }.not_to raise_error
+      end
+    end
+
+    context 'with invalid password' do
+      let(:name) { 'internet' }
+      let(:password) { 'dankogai' }
+      let(:invalid_password) { 'kogaidan' }
+      let(:user) { User.create_with_digest(name: name, password: password) }
+
+      it "succeeds authentication" do
+        expect { user.authenticate!(invalid_password) }.to raise_error(User::InvalidAuthority)
+      end
+    end
+  end
 end
