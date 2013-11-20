962ded75694b5ef655e43fbfbf9dc505f73f0632
aereal <aereal@aereal.org>
2013-11-20 15:29:47 +0900

    Add session spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,24 +1,6 @@
 PADRINO_ENV = 'test' unless defined?(PADRINO_ENV)
 require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
 
-RSpec.configure do |config|
-  config.include Rack::Test::Methods
-  config.include Capybara::DSL
-
-  config.before :suite do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before :each do
-    DatabaseCleaner.start
-  end
-
-  config.after :each do
-    DatabaseCleaner.clean
-  end
-end
-
 # You can use this method to custom specify a Rack app
 # you want rack-test to invoke:
 #
@@ -33,4 +15,31 @@ def app(app = nil, &blk)
   @app ||= Padrino.application
 end
 
-Capybara.app = app
+RSpec.configure do |config|
+  Capybara.app = Padrino.application
+  Capybara.javascript_driver = :poltergeist
+
+  app(Deary::App) do
+    helpers do
+      def matched_host?
+        true # XXX
+      end
+    end
+  end
+
+  config.include Rack::Test::Methods
+  config.include Capybara::DSL
+
+  config.before(:each) do
+    if Capybara.current_driver == :rack_test
+      DatabaseCleaner.strategy = :transaction
+      DatabaseCleaner.start
+    else
+      DatabaseCleaner.strategy = :truncation
+    end
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
