fa1daf358977b1405f22941ea2297f0ac3498b71
aereal <aereal@aereal.org>
2013-11-22 14:56:43 +0900

    Define User factory

--- app/policies/user_authenticator.rb
+++ app/policies/user_authenticator.rb
@@ -1,4 +1,23 @@
+require 'openssl'
+require 'securerandom'
+
 class UserAuthenticator
+  module DigestStrategy
+    ENCRYPT_METHOD = 'sha256'
+
+    def digest_for(salt: '', password: '')
+      OpenSSL::HMAC.hexdigest(ENCRYPT_METHOD, salt, password)
+    end
+    module_function :digest_for
+  end
+
+  module SaltStrategy
+    def generate
+      SecureRandom.urlsafe_base64(32)
+    end
+    module_function :generate
+  end
+
   def self.authenticate(user: nil, password: '')
     auth = Authentication.new(salt: user.password_salt, digest: user.password_digest)
     auth.authenticate(password)
